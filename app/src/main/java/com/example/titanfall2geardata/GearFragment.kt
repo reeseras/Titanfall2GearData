206b6079c4bca042cff22e6155a8e9114baed397
rrasmussen <d00382910@dmail.dixie.edu>
2020-02-28 10:44:52 -0700

    added GearDao (not sure if the file is correct), GearFragment.kt, and GearRepository.kt

---
+++ app/src/main/java/com/example/titanfall2geardata/GearFragment.kt
@@ -0,0 +1,119 @@
+package com.example.titanfall2geardata
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.Observer
+import java.util.*
+
+private const val TAG = "GearFragment"
+private const val ARG_GEAR_ID = "gear_id"
+
+class GearFragment : Fragment() {
+
+    private lateinit var gear: Gear
+    private lateinit var nameField: EditText
+    private lateinit var typeField: EditText
+
+    private val gearDetailViewModel: GearDetailViewModel by lazy {
+        ViewModelProviders.of(this).get(GearDetailViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        gear = Gear()
+        val gearId: UUID = arguments?.getSerializable(ARG_GEAR_ID) as UUID
+        // load gear from database:
+        gearDetailViewModel.loadGear(gearId)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_gear, container, false)
+
+        nameField = view.findViewById(R.id.gear_name) as EditText
+        typeField = view.findViewById(R.id.gear_type) as EditText
+
+        // dateButton.apply was here
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        gearDetailViewModel.gearLiveData.observe(
+            viewLifecycleOwner,
+            Observer { gear ->
+                gear?.let { // it: Gear
+                    this.gear = gear
+                    updateUI()
+                }
+            }
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val nameWatcher = object : TextWatcher {
+
+            override fun beforeTextChanged(
+                sequence:CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+
+            }
+
+            override fun onTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+                gear.name=sequence.toString()
+            }
+
+            override fun afterTextChanged(sequence: Editable?) {
+
+            }
+        }
+
+        nameField.addTextChangedListener(nameWatcher)
+
+        // solved checkbox change here
+    }
+
+    // saving changes
+    override fun onStop() {
+        super.onStop()
+        gearDetailViewModel.saveGear(gear)
+    }
+
+    private fun updateUI() {
+        nameField.setText(gear.name)
+        typeField.setText(gear.type)
+    }
+
+    companion object {
+
+        fun newInstance(gearId: UUID): GearFragment {
+            val args = Bundle().apply {
+                putSerializable(ARG_GEAR_ID, gearId)
+            }
+            return GearFragment().apply {
+                arguments = args
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/titanfall2geardata/GearRepository.kt
@@ -0,0 +1,52 @@
+package com.example.titanfall2geardata
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.Room
+import java.lang.IllegalStateException
+import java.util.*
+import java.util.concurrent.Executors
+
+private const val DATABASE_NAME = "gear-database"
+
+class GearRepository private constructor(context: Context) {
+
+    private val database : GearDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        GearDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    private val gearDao = database.gearDao()
+    private val executor = Executors.newSingleThreadExecutor()
+
+    fun getAllGear(): LiveData<List<Gear>> = gearDao.getAllGear()
+
+    fun getAGear(id: UUID): LiveData<Gear?> = gearDao.getAGear(id)
+
+    fun updateGear(gear: Gear) {
+        executor.execute {
+            gearDao.updateGear(gear)
+        }
+    }
+
+    fun addGear(gear: Gear) {
+        executor.execute {
+            gearDao.addGear(gear)
+        }
+    }
+
+    companion object {
+        private var INSTANCE: GearRepository? = null
+        fun initialize(context: Context) {
+            if (INSTANCE == null) {
+                INSTANCE = GearRepository(context)
+            }
+        }
+
+        fun get(): GearRepository {
+            return INSTANCE ?:
+                    throw IllegalStateException("GearRepository needs to be initialized - R")
+        }
+    }
+}
